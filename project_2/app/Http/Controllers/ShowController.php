39ddde875eed7c680eb8c9680acf8d8d5d39590c
Karmellinaa <kardor@onet.pl>
2023-10-17 15:51:02 +0200

    controler show and show_view

---
+++ project_2/app/Http/Controllers/ShowController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShowController extends Controller
+{
+    public function show(){
+        return 'Kontroler ShowController';
+    }
+
+    public function showView(){
+        $data = [
+            'firstName' => 'Jan',
+            'lastName' => 'Nowak',
+            'city' => 'Poznań',
+            'hobby' => ['siatkówka', 'żużel', 'fotografia']
+        ];
+        return View('table', $data);
+    }
+}
+
--- project_2/routes/web.php
+++ project_2/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('show', [\App\Http\Controllers\ShowController::class, 'show']);
+Route::get('show_view', [\App\Http\Controllers\ShowController::class, 'showView']);
